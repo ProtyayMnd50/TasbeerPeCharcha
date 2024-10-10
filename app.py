5146123225fbdc65a687989021dd4ec64b9f88e1
Protyay Mondal <protyaymondal32@gmail.com>
2024-10-10 17:22:44 +0530

    Defined deafult look of streamlit frontend

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 from PIL import Image
 import requests
 from io import BytesIO
+from transformers import ViltProcessor, ViltForQuestionAnswering
+
+st.title("Visual Question Answering")
+st.write("Upload an image and enter a question to get an answer.")
